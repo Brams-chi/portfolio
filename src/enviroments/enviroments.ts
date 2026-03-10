8df26b67fe70db4ed572ba03b67dc18ff5ea6d82
Braulio Melgarejo <braulio.melgarejo.s@gmail.com>
2026-03-10 15:32:21 -0300

    adding changes, fixing errors and texts

--- src/enviroments/enviroments.ts
+++ src/enviroments/enviroments.ts
@@ -2,8 +2,8 @@
 export const environment = {
     production: true,
     emailjs: {
-        publicKey: 'cjRkL5qQu_ElInFho',   // ← mismos valores
-        serviceId: 'service_gp22czr',
-        templateId: 'template_onk4t6i',
+        publicKey: '',
+        serviceId: '',
+        templateId: '',
     },
 };
